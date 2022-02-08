9a22415fb99d218657f6ece907acddd66710388e
Gyorgy Zsapka <gyorgy.zsapka@gmail.com>
2022-02-08 14:41:33 +0100

    feat: define gcloud helper commands as outputs
    to start, stop, describe the GCP instance

--- gcp/terraform/outputs.tf
+++ gcp/terraform/outputs.tf
@@ -17,3 +17,23 @@ output "SSH_Connection" {
   description = "Command to ssh into the instance"
   value       = format("ssh -i ${var.ssh_privatekey_path} ${var.user}@%s", google_compute_instance.default.network_interface.0.access_config.0.nat_ip)
 }
+
+output "gcloud_config" {
+  description = "Set gcloud compute zone config"
+  value       = "gcloud config set compute/zone ${var.zone}"
+}
+
+output "gcloud_status" {
+  description = "Get gcloud compute instance status"
+  value       = "gcloud compute instances describe ${var.instance_name} --format='yaml(name,status)'"
+}
+
+output "gcloud_start" {
+  description = "Start gcloud compute instance"
+  value       = "gcloud compute instances start ${var.instance_name}"
+}
+
+output "gcloud_stop" {
+  description = "Stop gcloud compute instance"
+  value       = "gcloud compute instances stop ${var.instance_name}"
+}
