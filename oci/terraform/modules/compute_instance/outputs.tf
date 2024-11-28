2bc27e2d0fe93331e23a20710759d4c4786fd6a2
zsapkagy <gyorgy.zsapka@gmail.com>
2024-11-28 15:34:48 +0100

    feat(oci): add outputs for easier ssh and .bashrc or .zshrc configuraion

--- oci/terraform/modules/compute_instance/outputs.tf
+++ oci/terraform/modules/compute_instance/outputs.tf
@@ -20,3 +20,29 @@ output "stop_script" {
   description = "Script to stop the instance"
   value       = "oci compute instance action --action SOFTSTOP --instance-id ${oci_core_instance.this.id}"
 }
+
+output "ssh_config" {
+  description = "SSH config"
+  value       = <<EOF
+Host OCI_${var.instance_name}
+  HostName ${oci_core_instance.this.public_ip}
+  PreferredAuthentications publickey
+  IdentityFile ${replace(var.ssh_public_key_path, ".pub", "")}
+  IdentitiesOnly yes
+  User ${var.os_user}
+EOF
+}
+
+output "bashrc_or_zshrc" {
+  description = "Bash or Zsh configuration"
+  value       = <<EOF
+#########
+# OCI WORKSTATION: ${var.instance_name}
+#########
+OCI_WORKSTATION_IP_${var.instance_name}="${oci_core_instance.this.public_ip}"
+OCI_WORKSTATION_INSTANCE_ID_${var.instance_name}="${oci_core_instance.this.id}"
+alias oci.${var.instance_name}.connect="ssh -i ${replace(var.ssh_public_key_path, ".pub", "")} ${var.os_user}@$OCI_WORKSTATION_IP_${var.instance_name}"
+alias oci.${var.instance_name}.run="oci compute instance action --action START --instance-id $OCI_WORKSTATION_INSTANCE_ID_${var.instance_name}"
+alias oci.${var.instance_name}.stop="oci compute instance action --action SOFTSTOP --instance-id $OCI_WORKSTATION_INSTANCE_ID_${var.instance_name}"
+EOF
+}
